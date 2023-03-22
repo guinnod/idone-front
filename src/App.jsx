aca5b3f1757fac70e2d93249d90e6edff3a58e7f
guinnod <210103064@stu.sdu.edu.kz>
2023-03-22 23:03:55 +0600

    invite people header

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { CreateButton } from "./components/ui/CreateButton"
-import { HomeButton } from "./components/ui/HomeButton"
-import { Logo } from "./components/ui/Logo"
-import { NotifyButton } from "./components/ui/NotifyButton"
-import { SearchInput } from "./components/ui/SearchInput"
-import { SideButton } from "./components/ui/SideButton"
+import { Header } from "./components/Header"
 
 function App() {
   return (
     <div className="App">
-      <Logo />
-      <CreateButton />
-      <HomeButton />
-      <SideButton />
-      <NotifyButton />
-      <SearchInput />
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import styles from '../styles/components/_header.module.scss'
+import { OtherUsers } from './OtherUsers'
+import { CreateButton } from "./ui/CreateButton"
+import { HomeButton } from "./ui/HomeButton"
+import { InviteButton } from './ui/InviteButton'
+import { Logo } from "./ui/Logo"
+import { NotifyButton } from "./ui/NotifyButton"
+import { SearchInput } from "./ui/SearchInput"
+import { SideButton } from "./ui/SideButton"
+import { UserPhoto } from "./ui/UserPhoto"
+export const Header = () => {
+    return (
+        <>
+            <div className={styles.anchor} >
+                <div>
+                    <SideButton />
+                    <HomeButton />
+                    <CreateButton />
+                </div>
+                <div>
+                    <Logo />
+
+                </div>
+                <div>
+
+                    <SearchInput />
+                    <NotifyButton />
+                    <UserPhoto />
+                </div>
+            </div>
+            <div className={styles.anchor}>
+                <div>
+                    Brackets
+                </div>
+                <div>
+                    <OtherUsers />
+                    <InviteButton />
+                </div>
+            </div>
+        </>
+    )
+}
