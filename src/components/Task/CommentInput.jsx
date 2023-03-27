72e9bece846db809ae830676e2f16cdc77dae20c
guinnod <210103064@stu.sdu.edu.kz>
2023-03-27 16:57:25 +0600

    task card

--- src/components/Task/CommentInput.jsx
+++ src/components/Task/CommentInput.jsx
@@ -1,14 +1,20 @@
-import styles from './styles/_task-comment.module.scss'
+import styles from './styles/_comment-input.module.scss'
 import arrowIcon from '../../assets/icons/fi_arrow-up.svg'
 import { UserPhoto } from '../Header/UserPhoto'
 
-export const CommentInput = ({ status }) => {
+export const CommentInput = () => {
 
     return (
         <div className={styles.anchor}>
-            <UserPhoto />
-            <input type="text" placeholder='Add a comment'/>
-            <img src={arrowIcon} alt="" />
+            <div className={styles.photo}>
+                <UserPhoto />
+            </div>
+
+            <div className={styles.area}>
+                <input type="text" placeholder='Add a comment' />
+                <img src={arrowIcon} alt="" />
+            </div>
+
         </div>
     )
 }
