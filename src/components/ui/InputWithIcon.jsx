7a39613c4174902b9f4f0e428e59f8dde73e13f7
guinnod <210103064@stu.sdu.edu.kz>
2023-05-08 05:03:57 +0600

    avatar

--- src/components/ui/InputWithIcon.jsx
+++ src/components/ui/InputWithIcon.jsx
@@ -1,10 +1,12 @@
 import styles from './styles/_input-with-icon.module.scss'
 
-export const InputWithIcon = ({ type, placeholder, src, style, onClick, id, name }) => {
+export const InputWithIcon = ({type, placeholder, src, style, onClick, id, name}) => {
     return (
         <div className={styles.anchor}>
-            <input type={type} placeholder={placeholder} style={style} id={id} name={name} />
-            <img src={src} alt="icon" onClick={onClick} />
+            <input type={type} value={placeholder} style={style} id={id} name={name} disabled/>
+            <div>
+                <img src={src} alt="icon" onClick={onClick}/>
+            </div>
         </div>
     )
 }
--- src/layouts/Profile.jsx
+++ src/layouts/Profile.jsx
@@ -3,42 +3,58 @@ import userPhoto from '@images/userPhoto.jpg'
 import editIcon from '@icons/edit.svg'
 import editGrayIcon from '@icons/edit_gray.svg'
 import colors from '@styles/colors.json'
-import { UserPhoto } from '@components/ui/UserPhoto'
-import { Window } from '@components/ui/Window'
-import { CircleButton } from '@components/ui/CircleButton'
-import { List } from '@components/ui/List'
-import { Button } from '@components/ui/Button'
-import { InputWithIcon } from '@components/ui/InputWithIcon'
+import {UserPhoto} from '@components/ui/UserPhoto'
+import {Window} from '@components/ui/Window'
+import {CircleButton} from '@components/ui/CircleButton'
+import {List} from '@components/ui/List'
+import {Button} from '@components/ui/Button'
+import {InputWithIcon} from '@components/ui/InputWithIcon'
+import {useState} from "react";
 
 export const Profile = () => {
     const inputs = [
-        { placeholder: 'Username', type: 'text' },
-        { placeholder: 'Email', type: 'email' },
-        { placeholder: 'Password', type: 'password' }
+        {placeholder: 'Full name', type: 'text'},
+        {placeholder: 'Email', type: 'email'},
+        {placeholder: 'Password', type: 'password'}
     ]
     const style = {
         'padding': '12px 18px',
         'width': '400px',
         'borderRadius': '5px'
     }
+    const [photo, setPhoto] = useState(userPhoto)
+    const handlePhotoUpload = () => {
+        const avatarPhoto = document.getElementById("avatarPhoto");
+        console.log(avatarPhoto.files)
+        if (avatarPhoto.files[0]) {
+            setPhoto(URL.createObjectURL(avatarPhoto.files[0]))
+        } else {
+            setPhoto(userPhoto)
+        }
+    }
     return (
         <Window title='Account'>
             <div className={styles.anchor}>
                 <List gap={20}>
                     <div className={styles.avatar}>
-                        <UserPhoto src={userPhoto} width='150px' />
-                        <span>
-                            <CircleButton svg={editIcon} color={colors.main} />
+                        <UserPhoto src={photo} width='150px'/>
+                        <span style={{cursor: "pointer"}}>
+                            <div style={{position: "relative", cursor: "pointer"}} >
+                            <input type={"file"} id="avatarPhoto" accept="image/png, image/jpg"
+                                   style={{opacity: 0, position: "absolute", width: '100%', height: '100%', cursor: "pointer"}} onChange={handlePhotoUpload}/>
+                            <CircleButton svg={editIcon} color={colors.main}/>
+                                </div>
                         </span>
                     </div>
                     <List>
                         {inputs.map((e, key) => {
                             return (
-                                <InputWithIcon key={key} placeholder={e.placeholder} type={e.type} src={editGrayIcon} style={style} />
+                                <InputWithIcon key={key} placeholder={e.placeholder} type={e.type} src={editGrayIcon}
+                                               style={style}/>
                             )
                         })}
                     </List>
-                    <Button style={{ width: '400px', borderRadius: '5px', background: colors.main }}>
+                    <Button style={{width: '400px', borderRadius: '5px', background: colors.main}}>
                         Save
                     </Button>
                 </List>
