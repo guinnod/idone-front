5f0c0a0b94909b0fa71bf629e732aa75f265e3d5
guinnod <210103064@stu.sdu.edu.kz>
2023-05-08 03:43:29 +0600

    confirm

--- src/pages/Confirm.jsx
+++ src/pages/Confirm.jsx
@@ -5,37 +5,59 @@ import {LoginInput} from "@Login/LoginInput";
 import {Button} from "@ui/Button";
 import {List} from "@ui/List";
 import {useState} from "react";
-import {LoginOptions} from "@Login/LoginOptions.jsx";
+import {post} from "@api/index.js";
 
 export const Confirm = ({isCreating}) => {
-
     const [isConfirming, setIsConfirming] = useState(true);
-    const [isFromLink, setIsFromLink] = useState(false);
-    const [isPasswordSame, setIsPasswordSame] = useState(true);
-    const handlePasswordSame = () => {
-        const passInput = document.getElementById("password");
-        const passRepInput = document.getElementById("passwordRepeat");
-        setIsPasswordSame(passInput.value === passRepInput.value)
+    const handleConfirm = () => {
+        post({path:'temp'})
+            .then(res=>{
+                console.log(res.data);
+                setIsConfirming(false);
+            })
+            .catch(error=>console.log(error))
+    }
+    const submitForm = () => {
+        const password = document.getElementById("password");
+        const passwordRepeat = document.getElementById("passwordRepeat");
+        if (password.value !== passwordRepeat.value) {
+            alert("Passwords don’t match")
+            return;
+        }
     }
     return (
         <div id="confirmForm" className={styles.anchor}>
             <Logo/>
-            <h1 className={styles.text}>{isCreating ? "Create an account" : "Reset password"}</h1>
+            <h1 className={styles.text}>Create an account</h1>
             <List gap={30}>
-                {!isFromLink ? <>
-                    <LoginInput toptext="Confirmation code" id="confirmation" name="confirmation"
-                                disabled={isConfirming}/>
-                    <Button style={{background: colors.main, width: '500px', color: 'white'}}>
-                        Check confirmation code
-                    </Button>
-                </> : <></>}
-                <LoginInput toptext="Create password" type="password" id="password" maxlength={"20"} minlength={"8"}/>
-                <LoginInput toptext="Repeat password" type="password" id="passwordRepeat" maxlength={"20"}
-                            minlength={"8"} style={{borderColor: isPasswordSame ? colors.gray : colors.red}}/>
-                <LoginOptions/>
-                <Button onClick={handlePasswordSame}
-                        style={{background: isConfirming ? colors.gray : colors.main, width: '500px', color: 'white'}}>
-                    Sign in
+                <LoginInput toptext="Confirmation code" id="confirmation" name="confirmation"
+                            style={{cursor: !isConfirming ? 'not-allowed' : 'text'}}
+                            disabled={!isConfirming}/>
+                <Button style={{
+                    background: !isConfirming ? colors.gray : colors.main,
+                    width: '500px',
+                    color: 'white',
+                    cursor: !isConfirming ? 'not-allowed' : 'pointer'
+                }}
+                        onClick={handleConfirm}>
+                    Check confirmation code
+                </Button>
+                <LoginInput toptext="Create password" type="password" id="password" maxLength={"20"} minLength={"8"}
+                            style={{cursor: isConfirming ? 'not-allowed' : 'text'}}
+                            disabled={isConfirming}/>
+                <LoginInput toptext="Repeat password" type="password" id="passwordRepeat" maxLength={"20"}
+                            minLength={"8"}
+                            style={{cursor: isConfirming ? 'not-allowed' : 'text'}}
+                            disabled={isConfirming}/>
+                <Button style={{
+                    background: isConfirming ? colors.gray : colors.main,
+                    width: '500px',
+                    color: 'white',
+                    cursor: isConfirming ? 'not-allowed' : 'pointer'
+                }}
+                        onClick={submitForm}
+                >
+                    Sign up
                 </Button>
             </List>
         </div>
